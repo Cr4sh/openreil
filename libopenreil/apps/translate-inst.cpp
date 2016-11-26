808ee79f253af65045c02c25898a1cb2259d9123
HyunminKo <hyunmin.ko4578@gmail.com>
2016-11-26 20:04:36 +0900

    Fix(Code): 'Set target architecture'

--- libopenreil/apps/translate-inst.cpp
+++ libopenreil/apps/translate-inst.cpp
@@ -46,7 +46,7 @@ int main(int argc, char *argv[])
         printf("[+] Using ARM architecture\n");
 
         // set target architecture
-        arch = ARCH_X86;
+        arch = ARCH_ARM;
     }
     else
     {
